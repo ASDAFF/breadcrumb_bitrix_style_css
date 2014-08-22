425a2b4f37a9e4a9bfb4e50160bb68c129c9fdeb
тащке <laikisounds@gmail.com>
2014-08-22 10:39:30 +0400

    files update

---
+++ README.md
@@ -0,0 +1,12 @@
+## Проблема с цепочкой навигации? Добавляем параметр 
+
+	"ADD_SECTIONS_CHAIN" => "(N | Y)"
+
+Либо в section, либо в element
+
+## Нужны другие стили оформления?
+
+Берем их (отсюда)[http://red-team-design.com/css3-breadcrumbs/]
+
+и кидаем в style, изменяя id div
+
--- breadcrumb/breadcr/template.php
+++ breadcrumb/breadcr/template.php
@@ -1,44 +1,5 @@
- <?
-// if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-// if(empty($arResult))
-// 	return "";
-
-// $strReturn = '<div class="breadcrumbs-three"><ul>';
-
-// for($index = 0, $itemSize = count($arResult); $index < $itemSize; $index++)
-// {
-// 	if($index > 0)
-// 		// $strReturn .= ' <span>-</span> ';
-
-// 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
-// 	if($arResult[$index]["LINK"] <> "")
-// 		if($itemSize == ($index+1)) {
-// 			$strReturn .= '<li><a href="/">'.$title.'</a></li>';
-// 		} else {
-// 			$strReturn .= ' <li><a href="'.$arResult[$index]["LINK"].'" title="'.$title.'">'.$title.'</a></li> ';
-// 		}
-// 	else
-// 		$strReturn .= $title;
-// }
-
-// $strReturn .= '</ul></div>';
-// return $strReturn;
- ?>
-
- <!-- <div id="crumbs">
-// 	<ul>
-// 		<li><a href="#1">One</a></li>
-// 		<li><a href="#2">Two</a></li>
-// 		<li><a href="#3">Three</a></li>
-// 		<li><a href="#4">Four</a></li>
-// 		<li><a href="#5">Five</a></li>
-// 	</ul>
-// </div> -->
-
 <?
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-//delayed function must return a string
 __IncludeLang(dirname(__FILE__).'/lang/'.LANGUAGE_ID.'/'.basename(__FILE__));
 $curPage = $GLOBALS['APPLICATION']->GetCurPage($get_index_page=false);
 if ($curPage != SITE_DIR)
@@ -51,17 +12,16 @@ if(empty($arResult))
 $strReturn = '<div class="nav-chain"><ul id="breadcrumbs-three"><li><a title="'.GetMessage('BREADCRUMB_MAIN').'" href="'.SITE_DIR.'">Главная</a><li>';
 for($index = 0, $itemSize = count($arResult); $index < $itemSize; $index++)
 {
-   // $strReturn .= ' <span class="Del">&raquo;</span> ';
+   // $strReturn .= ' <span class="Del">&raquo;</span> '; // Если нужен разделитель
 
    $title = htmlspecialcharsex($arResult[$index]["TITLE"]);
    
    if($arResult[$index]["LINK"] <> ""&&$index<(count($arResult)-2))
-   	$strReturn .= '<li><a href="'.$arResult[$index]["LINK"].'" title="'.$title.'">'.$title.'</a></li>';
+      $strReturn .= '<li><a href="'.$arResult[$index]["LINK"].'" title="'.$title.'">'.$title.'</a></li>';
    else
-   	$title = (strlen($title) > 60) ? substr($title,0,60).'...' : $title;
-   	$strReturn .= '<li><p>'.$title.'</p></li>';
+      $title = (strlen($title) > 60) ? substr($title,0,60).'...' : $title;
+      $strReturn .= '<li><p>'.$title.'</p></li>';
 }
-//
 $strReturn .= '</ul></div>';
 return $strReturn;
 ?>
